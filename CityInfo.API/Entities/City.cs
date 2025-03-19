f284784d1511f5622b1f4411f65184a419a92d4d
Ilona Popa <ilona.popa@theaccessgroup.com>
2025-03-19 19:50:21 +0200

    created EntityClasses, DbContext, migrations & seeded database with data

---
+++ CityInfo.API/Entities/City.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CityInfo.API.Entities
+{
+    public class City
+    {
+        [Key]
+        /* Options:
+         *None - for no generation
+         *Identity - for generation on add
+         *Computed - for generation on add or update */
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [MaxLength(200)]
+        public string? Description { get; set; }
+
+        public ICollection<PointOfInterest> PointsOfInterest { get; set; }
+            = new List<PointOfInterest>();
+
+        public City(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ CityInfo.API/Entities/PointOfInterest.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace CityInfo.API.Entities
+{
+    public class PointOfInterest
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [MaxLength(200)]
+        public string? Description { get; set; }
+
+        [ForeignKey("CityId")]
+
+        //navigation property => used to establish a relationship between two entities
+        /* Relationships that are discovered by convention will always target the primary key of 
+        the principal entity => the id of the city will be the foreign key in the point of interest table */
+        public City? City { get; set; }
+
+        //not required to define the foreign key property on the dependent class, only for clarity
+        public int CityId { get; set; }
+
+        public PointOfInterest(string name)
+        {
+            Name = name;
+        }
+    }
+}
